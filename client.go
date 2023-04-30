50c1d272375e47c858cd00e1eda741bfbec9bd71
ScmTble <ScmTble@qq.com>
2023-04-30 23:17:29 +0800

    feta: add get recommend offset

--- client.go
+++ client.go
@@ -1,17 +1,3 @@
-// Copyright 2022 gorse Project Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package gorse
 
 import (
@@ -49,6 +35,10 @@ func (c *GorseClient) ListFeedbacks(ctx context.Context, feedbackType, userId st
 
 func (c *GorseClient) GetRecommend(ctx context.Context, userId string, category string, n int) ([]string, error) {
 	return request[[]string, any](ctx, c, "GET", c.entryPoint+fmt.Sprintf("/api/recommend/%s/%s?n=%d", userId, category, n), nil)
+}
+
+func (c *GorseClient) GetRecommendOffset(ctx context.Context, userId string, category string, n, offset int) ([]string, error) {
+	return request[[]string, any](ctx, c, "GET", c.entryPoint+fmt.Sprintf("/api/recommend/%s/%s?n=%d&offset=%d", userId, category, n, offset), nil)
 }
 
 func (c *GorseClient) SessionRecommend(ctx context.Context, feedbacks []Feedback, n int) ([]Score, error) {
